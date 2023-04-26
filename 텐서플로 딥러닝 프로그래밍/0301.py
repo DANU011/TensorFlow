193bc522ef676d6963c339b5e867445cd0208695
DANU011 <122770856+DANU011@users.noreply.github.com>
2023-04-26 10:35:27 +0900

    2.0+

--- 텐서플로 딥러닝 프로그래밍/0301.py
+++ 텐서플로 딥러닝 프로그래밍/0301.py
@@ -1,22 +1,25 @@
-from tensorflow.python.framework.ops import disable_eager_execution
-import tensorflow as tf
+# from tensorflow.python.framework.ops import disable_eager_execution
+# import tensorflow as tf
+#
+# print(tf.executing_eagerly())
+# disable_eager_execution()
+# print(tf.executing_eagerly())
+#
+# # Graph construction
+# a = tf.constant(1)
+# b = tf.constant(2)
+# c = a + b  # c= tf.add(a, b)
+# print(a)
+# print(b)
+# print(c)
+#
+#
+# #2 graph execution
+# sess = tf.compat.v1.Session()
+# print(sess.run(a))
+# print(sess.run(b))
+# print(sess.run(c))
+# sess.close()
 
-print(tf.executing_eagerly())
-disable_eager_execution()
-print(tf.executing_eagerly())
-
-# Graph construction
-a = tf.constant(1)
-b = tf.constant(2)
-c = a + b  # c= tf.add(a, b)
-print(a)
-print(b)
-print(c)
- 
-
-#2 graph execution
-sess = tf.compat.v1.Session()
-print(sess.run(a))
-print(sess.run(b))
-print(sess.run(c))
-sess.close()
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
